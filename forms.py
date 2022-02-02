9c2e53043a1de67420f5e4d1e2204e701351509b
Noah <noah00palm@gmail.com>
2022-02-02 17:15:02 -0500

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,9 @@
+# accounts/forms.py
+from django.forms import ModelForm
+from .models import Order
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
